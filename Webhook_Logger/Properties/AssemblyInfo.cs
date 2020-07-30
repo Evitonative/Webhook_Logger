ff52634658fc8193fae445c7a7ae8a134bbfb85c
Finnjonn <47825249+Finnjonn@users.noreply.github.com>
2020-07-30 19:27:21 +0200

    Fixed Some Settings

--- Webhook_Logger/Properties/AssemblyInfo.cs
+++ Webhook_Logger/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SilentEcho-Plugin")]
+[assembly: AssemblyTitle("Webhook_Logger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Evitonative")]
-[assembly: AssemblyProduct("SilentEcho-Plugin")]
+[assembly: AssemblyProduct("Webhook_Logger")]
 [assembly: AssemblyCopyright("Copyright © Evitonative 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
